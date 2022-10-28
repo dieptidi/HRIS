955d8e2b2cdd2e9e26d3502b1c5cc24d047df33b
edoprayogo <edoprayogo8@gmail.com>
2022-10-28 13:46:30 +0700

    add upsert candidate model & helper format name & format rupiah

--- HRIS.ViewModel/Candidates/CandidateUpsertVM.cs
+++ HRIS.ViewModel/Candidates/CandidateUpsertVM.cs
@@ -1,20 +1,20 @@
-﻿using HRIS.DataAccess.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HRIS.ViewModel.Candidates
 {
-    public class CandidateGridVM
+    public class CandidateUpsertVM
     {
         public string Id { get; set; }
-        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Gender { get; set; }
-        public string BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string BirthPlace { get; set; }
         public string IdcardNumber { get; set; }
         public string JobApply { get; set; }
-        public string ApplyDate { get; set; }
-        public string SallaryRequest { get; set; }
+        public DateTime ApplyDate { get; set; }
+        public decimal SallaryRequest { get; set; }
     }
 }
