aa33bc9dde00780854d081db48fc9797590f1aca
dieptidi <dwish.rezpector@gmail.com>
2022-10-28 14:12:32 +0700

    resolve conflict

--- HRIS.Provider/Helpers/Helper.cs
+++ HRIS.Provider/Helpers/Helper.cs
@@ -13,6 +13,25 @@ namespace HRIS.Provider.Helpers
             return date.ToString("dd MMM yyyy", CultureInfo.CreateSpecificCulture("id-ID"));
         }
 
+        public static string FormatName(string firstName, string lastName, string gender)
+        {
+            if (gender.ToUpper() == "M")
+            {
+                string title = "Mr";
+                return $"{title}. {firstName} {lastName}";
+            }
+            else
+            {
+                string title = "Ms";
+                return $"{title}. {firstName} {lastName}";
+            }
+        }
+
+        public static string FormatRupiah(decimal money)
+        {
+            return money.ToString("c", CultureInfo.CreateSpecificCulture("id-ID"));
+        }
+
         /// <summary>
         /// Mendapatkan seluruh properties dari sebuah object
         /// </summary>
